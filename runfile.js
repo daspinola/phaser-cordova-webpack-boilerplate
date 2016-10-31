e7647e185ffec5553bb5a4f336ee68acf0626cbe
Diogo Santos <Diogo.Santos@exictos.com>
2016-10-31 16:01:51 +0000

    Add copy and server scripts

--- runfile.js
+++ runfile.js
@@ -11,12 +11,21 @@ const task = {
   'start': () => {
     task['cordova:base']();
   },
+  'server': () => {
+    run('webpack-dev-server');
+  },
   'cordova:base': () => {
     run('mkdir cordova', async);
     run('mkdir platforms', cordovaFolder);
     run('mkdir www', cordovaFolder);
     run('cp -a config.xml ./cordova');
     task['platform-android']();
+  },
+  'cordova:copy': () => {
+    run('cp index.html ./cordova/www');
+    run('cp base.css ./cordova/www');
+    run('cp -a ./assets ./cordova/www');
+    run('webpack -p');    
   },
   'platform-android': () => {
     run('cordova platform add android', cordovaFolder);
